514184de10ba288bb8a7cd8a56a596f15d058bba
Aaron Ogle <aaron@geekgonecrazy.com>
2023-02-24 18:20:17 -0600

    Fix boot.cmd memory allocations
    Was loading kernel into 0x46000000 which was 48062976 bytes in
    size.  Then subsequently loading the device tree into 0x49000000
    
    Kernel was taking from 0x46000000 -> 0x94062976, and then having that
    memory overwrote starting at 0x49000000 with the device tree which was
    23401 bytes.
    
    This switches that up and starts loading the kernel at 0x40000000 to
    0x48062976 then proceeds to load the device tree at 0x490000000.
    Allowing both to exist fully in memory

--- boot.cmd
+++ boot.cmd
@@ -1,9 +1,12 @@
-setenv bootargs "console=ttySAC2,115200n8 root=/dev/mmcblk0p2 rootwait panic=10 oops=panic init=/gokrazy/init"
-setenv stdout=serial,vga
-setenv stderr=serial,vga
+setenv bootargs "rootwait root=/dev/mmcblk0p2 init=/gokrazy/init"
 
-fatload mmc 0 0x46000000 vmlinuz
+setenv stdout serial
+setenv stderr serial
 
+# Loading kernel into 0x40000000 seems to be earliest unprotected to use
+fatload mmc 0 0x40000000 vmlinuz
+
+# Loading device tree into 0x49000000 the kernel is currently right under 49000000 bytes in size so if we use earlier we will overwrite part of it
 fatload mmc 0 0x49000000 sun50i-h6-tanix-tx6.dtb
 
-booti 0x46000000 - 0x49000000
+booti 0x40000000 - 0x49000000
